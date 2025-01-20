fd837398afdfa85b1cddc5837f0ddb8215f1d420
doyoon <doyoonkim21cm@gmail.com>
2025-01-20 21:47:54 +0900

    add limited.jsp

--- src/main/java/com/eventopia/rank/LimitedC.java
+++ src/main/java/com/eventopia/rank/LimitedC.java
@@ -1,28 +1,37 @@
 package com.eventopia.rank;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/RankRegBC")
-public class RankRegBC extends HttpServlet {
+import com.google.gson.Gson;
+
+@WebServlet("/LimitedC")
+public class LimitedC extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		RankDAO.RDAO.productAllSelect(request, response);
+		  
+		request.setAttribute("content", "jsp/rank/limited.jsp");
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		RankDAO.RDAO.rankAdd2(request);
-
-		response.sendRedirect("RankPageC");
+		RankDAO.RDAO.productAllSelect(request, response);
+		RankDAO.RDAO.rankCount(request);
 		
-		
+		request.setAttribute("content", "jsp/rank/limited.jsp");
+		request.getRequestDispatcher("index.jsp").forward(request, response);
 		
 	}
 
